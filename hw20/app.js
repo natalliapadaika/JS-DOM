f6b7a31f6d63fea3c14848dfa6619bea5f3179ac
natalliapadaika <139593186+natalliapadaika@users.noreply.github.com>
2023-11-09 04:27:28 +0300

    Add files via upload

--- hw20/app.js
+++ hw20/app.js
@@ -1,46 +1,36 @@
-const konvert = document.querySelector('.konvert'),
-      input = konvert.querySelector('input'),
-      select = konvert.querySelector('select'),
-  
-      bazeVal = konvert.querySelector('#bazeVal'),
-      itog = konvert.querySelector('#itogo');
+const konvert = document.querySelector(".konvert"),
+  input = konvert.querySelector("input"),
+  select = konvert.querySelector("select"),
+  bazeVal = konvert.querySelector("#bazeVal"),
+  itog = konvert.querySelector("#itogo");
 
-      const getResult = (Object, key) =>{
-  const result = (+input.value * Object[key]).toFixed(4)
-  itog.textContent = result 
-        };
-  
+const getResult = (Object, key) => {
+  const result = (+input.value * Object[key]).toFixed(4);
+  itog.textContent = result;
+};
 
-      const getKonvert = async (val) => {
-        bazeVal.textContent = val;
+const getKonvert = async (val) => {
+  bazeVal.textContent = val;
 
+  try {
+    const response = await fetch(`https://open.er-api.com/v6/latest/${val}`);
+    if (!response.ok) {
+      throw new Error("Ошибка статус-кода");
+    }
+    const api = await response.json();
+    const { rates } = api;
 
-        try{
-            const response = await fetch(`https://open.er-api.com/v6/latest/${val}`)
-            if(!response.ok){
-                throw new Error ('Ошибка статус-кода')
-        }
-        const api = await response.json();
-        
-        const {rates} = api;
+    input.addEventListener("keyup", () => {
+      getResult(rates, select.value);
+    });
 
-         
-  input.addEventListener('keyup', () =>{
-
-       getResult(rates, select.value);
-});
+    select.addEventListener("change", () => {
+      getResult(rates, select.value);
+    });
     
-select.addEventListener ('change', () => {
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
-  getResult(rates, select.value);
-  
-
-})  
-       
-        }
-        catch(error){
-            console.error(error.message);
-        }
-      }
-
-getKonvert('USD');
+getKonvert("USD");
